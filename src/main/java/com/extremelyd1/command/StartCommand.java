dcf1fb0dc1087cea72dc310555bb8905ed612f4b
Joe Shufflebotham <joe.shufflebotham13@gmail.com>
2022-04-14 19:59:49 +0100

    add safety to start command

--- src/main/java/com/extremelyd1/command/StartCommand.java
+++ src/main/java/com/extremelyd1/command/StartCommand.java
@@ -2,13 +2,21 @@ package com.extremelyd1.command;
 
 import com.extremelyd1.game.Game;
 import com.extremelyd1.util.CommandUtil;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
-public class StartCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class StartCommand implements TabExecutor {
 
     /**
      * The game instance
@@ -20,7 +28,7 @@ public class StartCommand implements CommandExecutor {
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (!CommandUtil.checkCommandSender(sender, true, true)) {
             return true;
         }
@@ -35,11 +43,36 @@ public class StartCommand implements CommandExecutor {
         }
 
         if (sender instanceof Player) {
-            game.start((Player) sender);
+            boolean allPlayersOnTeam = true;
+
+            for (Player p: Bukkit.getOnlinePlayers()) {
+                if (game.getTeamManager().getTeamByPlayer(p) == null) {
+                    p.sendMessage(ChatColor.RED + "You need to select a team!");
+                    allPlayersOnTeam = false;
+                }
+            }
+
+            boolean confirmedCommand = args.length > 0 && args[0] == "confirm";
+
+            if (!allPlayersOnTeam && !confirmedCommand) {
+                sender.sendMessage(ChatColor.RED + "Not all players are on a team. Run /start confirm to continue regardless.");
+            } else {
+                game.start((Player) sender);
+            }
         } else {
             game.start();
         }
 
         return true;
+    }
+
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] args) {
+        if (args.length == 1) {
+            return Arrays.asList("confirm");
+        }
+
+        return emptyList();
     }
 }
